6d8e687530c52403be00d791a59589002e6ff167
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2023-10-10 22:05:36 +0900

    add 8

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 'use client'
 import React, { useState } from 'react';
 import Thanks from './thanks/page';
+import styles from './page.module.css'
+
+
 
 const App = () => {
   const [rating, setRating] = useState(0);
@@ -21,41 +24,38 @@ const App = () => {
 
 
   return (
-    <div>
+    <div className={styles.main}>
       {visible
         ? <div>
-            <div>
-              <p>本クリニックのサービスの満足度について５段階評価でご協力お願いします。</p>
+            <div className={styles.card}>
+              <p className={`${styles.card} ${styles.center}`}>本クリニックのサービスの満足度について５段階評価でご協力お願いします。</p>
+              <div className={styles.center}>
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <span
+                    key={star}
+                    onClick={() => handleClick(star)}
+                    className={`${styles.star} ${star <= rating ? styles.gold : ''} ${halfRating ? styles.silver : ''}`}
+                  >
+                    &#9733;
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className={styles.center}>
+              <button className={styles.button} onClick={() => setVisible(!visible)}>回答する</button>
+            </div>
+          </div>
+        : <div>
+            <div className={styles.center}>
               {[1, 2, 3, 4, 5].map((star) => (
                 <span
                   key={star}
-                  onClick={() => handleClick(star)}
-                  style={{
-                    cursor: 'pointer',
-                    color: star <= rating ? 'gold' : 'gray',
-                    ...(halfRating && { color: 'silver' }),
-                  }}
+                  className={`${styles.star} ${star <= rating ? styles.gold : ''} ${halfRating ? styles.silver : ''}`}
                 >
                   &#9733;
                 </span>
               ))}
             </div>
-            {/* <p>Rating: {rating} stars</p> */}
-            <button onClick={() => setVisible(!visible)}>回答する</button>
-          </div>
-        : <div>
-            {[1, 2, 3, 4, 5].map((star) => (
-              <span
-                key={star}
-                style={{
-                  cursor: 'pointer',
-                  color: star <= rating ? 'gold' : 'gray',
-                  ...(halfRating && { color: 'silver' }),
-                }}
-              >
-                &#9733;
-              </span>
-            ))}
             {highRating
               ?<div><Thanks/></div>
               :<div>ご協力ありがとうございました。</div>
--- app/thanks/page.jsx
+++ app/thanks/page.jsx
@@ -1,9 +1,10 @@
-
+import styles from "../page.module.css"
 
 const Thanks =()=> {
     return <>
-                <a href="https://github.com/etk-development/meo">
-                    <button>
+                <a
+                    href="https://github.com/etk-development/meo">
+                    <button  className={styles.button}>
                         こちらでレビューのご協力お願いします。
                     </button>
                 </a>
