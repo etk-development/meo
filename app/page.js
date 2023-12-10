d666c463d88c848b57cc4ce83e1ee2c34d1b6685
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2023-12-10 15:36:40 +0900

    edit code

--- app/page.js
+++ app/page.js
@@ -1,30 +1,58 @@
 'use client'
 import React, { useState } from 'react';
-import Thanks from './thanks/page';
-import styles from './page.module.css'
+import styles from './page.module.css';
+
+import data from "../qa.json" assert { type: "json" };
 
 
 
 const App = () => {
-  const [rating, setRating] = useState(0);
-  const [highRating, setHighRating] = useState(false);
-  const [halfRating, setHalfRating] = useState(false);
-  const [visible, setVisible] = useState(true);
-
-  const handleClick = (newRating) => {
-    if (newRating === 0) {
-      setHalfRating(false);
-    } else if (newRating === 1) {
-      setHalfRating(true);
-    } else {
-      setRating(newRating);
-      setHighRating(newRating >= 4);
-    }
-  };
-
+  const url = "https://script.google.com/macros/s/AKfycbyUJK5jAao0AmMRcEdTBTNgQQ1LUbsgwKtaCbdOKKU8Wrp81R8DZKtoWVLxzAb1UU0DKA/exec"
 
   return (
     <div className={styles.main}>
+      <form method="post" action={url}>
+        <div>
+          {data.map(d =><>
+            <div><h2 key={d.title}>{d.title}</h2><p>必須</p></div>
+            <p><label htmlFor={d.id5}>{d.rank5}<input type="radio" id={d.id5} name={d.name} value={d.rank5} required/> </label></p>
+            <p><label htmlFor={d.id4}>{d.rank4}<input type="radio" id={d.id4} name={d.name} value={d.rank4} required/> </label></p>
+            <p><label htmlFor={d.id3}>{d.rank3}<input type="radio" id={d.id3} name={d.name} value={d.rank3} required/> </label></p>
+            <p><label htmlFor={d.id2}>{d.rank2}<input type="radio" id={d.id2} name={d.name} value={d.rank2} required/> </label></p>
+            <p><label htmlFor={d.id1}>{d.rank1}<input type="radio" id={d.id1} name={d.name} value={d.rank1} required/> </label></p>
+          </>
+          )}
+        </div>
+        <button id="submit-button" type="submit" >送信</button>
+      </form>
+      
+
+      {/* // const [rating, setRating] = useState(0);
+  // const [highRating, setHighRating] = useState(false);
+  // const [halfRating, setHalfRating] = useState(false);
+  // const [visible, setVisible] = useState(true);
+
+
+  // const handleClick = (newRating) => {
+  //   if (newRating === 0) {
+  //     setHalfRating(false);
+  //   } else if (newRating === 1) {
+  //     setHalfRating(true);
+  //   } else {
+  //     setRating(newRating);
+  //     setHighRating(newRating >= 4);
+  //   }
+  // }
+  // const changeName = (e) => {
+  //   setName(e.target.value);
+  // }
+  // const changeAnimal = (e) => {
+  //   setAnimal(e.target.value);
+  // }
+  // const changeSex = (e) => {
+  //   setSex(e.target.value);
+  // }
+      
       {visible
         ? <div>
             <div className={styles.card}>
@@ -61,7 +89,7 @@ const App = () => {
               :<div>ご協力ありがとうございました。</div>
             }
           </div>
-      }
+      } */}
     </div>
   );
 };
